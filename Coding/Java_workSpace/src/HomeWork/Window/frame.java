0f4b37984f854b7cb4d31389f682396a8bbc5c98
zhoooou <zhouzhen418@gmail.com>
2023-11-29 20:30:22 +0800

    增加学生信息输入

--- Coding/Java_workSpace/src/HomeWork/Window/frame.java
+++ Coding/Java_workSpace/src/HomeWork/Window/frame.java
@@ -14,6 +14,8 @@ public class frame extends JFrame {
         JPasswordField userPassword = new JPasswordField(10);
         // 确认登录
         JButton loginButton = new JButton("确认登录");
+        // 输入学生信息
+        JButton enterInformatinButton = new JButton("输出学生信息");
 
         // 创建横向盒子1，包含用户名标签和用户名文本框
         Box hBox1 = Box.createHorizontalBox();
@@ -29,6 +31,9 @@ public class frame extends JFrame {
         Box hBox3 = Box.createHorizontalBox();
         hBox3.add(loginButton);
 
+        Box hBox4 = Box.createVerticalBox();
+        hBox4.add(enterInformatinButton);
+
         // 创建整体纵向盒子
         Box vBox1 = Box.createVerticalBox();
         vBox1.add(hBox1);
@@ -36,6 +41,7 @@ public class frame extends JFrame {
         vBox1.add(hBox2);
         vBox1.add(Box.createHorizontalStrut(10));
         vBox1.add(hBox3);
+        vBox1.add(hBox4);
 
         setTitle("学生管理系统");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -54,6 +60,13 @@ public class frame extends JFrame {
                 } else {
                     JOptionPane.showMessageDialog(frame.this, "登录失败，请检查用户名和密码！", "错误", JOptionPane.ERROR_MESSAGE);
                 }
+            }
+        });
+
+        enterInformatinButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                enterInformatin();
             }
         });
     }
@@ -78,5 +91,29 @@ public class frame extends JFrame {
 
         // 刷新窗口
         revalidate();
+    }
+
+    public void enterInformatin() {
+        // 输入学生姓名
+        JTextField studentName = new JTextField();
+        // 输入学生学号
+        JTextField studentID = new JTextField();
+        // 输入学生成绩
+        JTextField studentScore = new JTextField();
+
+        Box hBox1 = Box.createHorizontalBox();
+        hBox1.add(new JLabel("学生姓名"));
+        hBox1.add(Box.createHorizontalStrut(10));
+        hBox1.add(studentName);
+
+        Box hBox2 = Box.createHorizontalBox();
+        hBox1.add(new JLabel("学号"));
+        hBox1.add(Box.createHorizontalStrut(10));
+        hBox1.add(studentID);
+
+        Box hBox3 = Box.createHorizontalBox();
+        hBox1.add(new JLabel("成绩"));
+        hBox1.add(Box.createHorizontalStrut(10));
+        hBox1.add(studentScore);
     }
 }
