23af363175198e7239870f9badb81d4278a6e384
zhoooou <zhouzhen418@gmail.com>
2023-12-17 17:14:47 +0800

    提交

--- Coding/Java_workSpace/src/HomeWork/Abormal/Application.java
+++
@@ -1,9 +0,0 @@
-package src.HomeWork.Abormal;
-
-public class Application {
-    public static void main(String[] args) {
-        Simulator simulator = new Simulator();
-        simulator.playSound(new Dog());
-        simulator.playSound(new Cat());
-    }
-}
